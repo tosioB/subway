3735bbff3a00ff9a58115256faace11945330171
kimdaesik <ds500033@naver.com>
2025-03-10 17:14:35 +0900

    vercel 연결 확인

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>서브웨이</h1>
+      <h1>서브웨이/vercel 연결확인</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
